d471210a4a9613bf8b6327dfe1051c810b3beccb
UBC Student <jearstar@students.cs.ubc.ca>
2021-08-20 10:31:09 -0700

    another attempt

--- main/ui/main.java
+++ main/ui/main.java
@@ -3,6 +3,6 @@ package ui;
 public class main {
     public static void main(String[] args) {
         WeatherApp wa = new WeatherApp();
-        System.out.println("what is going on...");
+        System.out.println("oh i can make a pull request in in VCS");
     }
 }
