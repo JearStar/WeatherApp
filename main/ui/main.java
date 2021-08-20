7b0a7ad2f0d27022320b2de942179f09997063f4
UBC Student <jearstar@students.cs.ubc.ca>
2021-08-20 10:25:29 -0700

    another attempt

--- main/ui/main.java
+++ main/ui/main.java
@@ -3,6 +3,6 @@ package ui;
 public class main {
     public static void main(String[] args) {
         WeatherApp wa = new WeatherApp();
-        System.out.println("Parker has perms to merge on github???");
+        System.out.println("what is going on...");
     }
 }
