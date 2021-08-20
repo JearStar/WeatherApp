001051556f9bc359994892589fb6e5d75e7adb7a
UBC Student <jearstar@students.cs.ubc.ca>
2021-08-20 10:20:24 -0700

    Parker has perms to merge?

--- main/ui/main.java
+++ main/ui/main.java
@@ -3,5 +3,6 @@ package ui;
 public class main {
     public static void main(String[] args) {
         WeatherApp wa = new WeatherApp();
+        System.out.println("Parker has perms to merge on github");
     }
 }
