17c88afdcdd85f1c95cb8cc053ea9307c349137f
UBC Student <jearstar@students.cs.ubc.ca>
2021-08-20 09:28:20 -0700

    Added parsing methods

--- main/model/JSONParser.java
+++ main/model/JSONParser.java
@@ -1,4 +1,34 @@
 package model;
 
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+
 public class JSONParser {
+    public static String API_KEY = "bdae2e3555fb4a1eadb153333212008";
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
 }
