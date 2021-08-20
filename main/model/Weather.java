7a5e08ba9b1708cdec9fc1bc50684c5e2adb243d
UBC Student <jearstar@students.cs.ubc.ca>
2021-08-20 14:57:36 -0700

    Forecast

--- main/model/Weather.java
+++ main/model/Weather.java
@@ -1,4 +1,22 @@
 package model;
 
+import org.json.JSONObject;
+
+import java.net.URL;
+
 public class Weather {
+    String lastUpdated;
+    float tempC;
+    float tempF;
+    int isDay;
+    String condition;
+    URL icon;
+    float windMph;
+    float windKph;
+    int windDegree;
+    String windDir;
+
+    public Weather(JSONObject object) {
+
+    }
 }
