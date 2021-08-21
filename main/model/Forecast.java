64088a513a5ef81b7477847a0cac8209a4d38b54
Parker Lee <parkerlee247@gmail.com>
2021-08-20 20:27:42 -0700

    Cleaned up Forecast

--- main/model/Forecast.java
+++ main/model/Forecast.java
@@ -4,19 +4,13 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Forecast {
     private Location location;
     private Weather current;
-    private List<Weather> currentForecast;
-    private HashMap<String, List<Weather>> extendedForecast;
-
-    //astro
-    private String sunrise;
-    private String sunset;
-    private String moonPhase;
+    private List<ForecastHour> currentForecast;
+    private HashMap<String, ForecastDay> extendedForecast; //includes current day
 
     public Forecast(JSONObject jsonObject) {
         JSONObject fcObject = jsonObject.getJSONObject("forecast");
@@ -25,24 +19,10 @@ public class Forecast {
         this.current = new Weather(jsonObject);   //current weather
 
         for (Object o : fcdArray) {
-            List<Weather> hourlyWeather = new LinkedList<>();
-            JSONObject dateObject = (JSONObject) o;
-
-            String date = dateObject.getString("date");
-
-            JSONArray hourObject = dateObject.getJSONArray("hour");
-            for (Object ob : hourObject) {
-                hourlyWeather.add((Weather) ob);
-            }
-
-            this.extendedForecast.put(date, hourlyWeather);     //keys: dates, values: a list of the hourly forecast on
-                                                                // that day. Includes the current day
+            String date = ((JSONObject) o).getString("date");
+            ForecastDay fcd = new ForecastDay((JSONObject) o);
+            this.extendedForecast.put(date, fcd);
         }
-
-        JSONObject astroObject = jsonObject.getJSONObject("astro");
-        this.sunrise = astroObject.getString("sunrise");
-        this.sunset = astroObject.getString("sunset");           //astro information only for current day
-        this.moonPhase = astroObject.getString("moonPhase");
     }
 
     //getters
@@ -52,19 +32,10 @@ public class Forecast {
     public Weather getCurrent() {
         return current;
     }
-    public List<Weather> getCurrentForecast() {
+    public List<ForecastHour> getCurrentForecast() {
         return currentForecast;
     }
-    public HashMap<String, List<Weather>> getExtendedForecast() {
+    public HashMap<String, ForecastDay> getExtendedForecast() {
         return extendedForecast;
-    }
-    public String getSunrise() {
-        return sunrise;
-    }
-    public String getSunset() {
-        return sunset;
-    }
-    public String getMoonPhase() {
-        return moonPhase;
     }
 }
---
+++ main/model/ForecastDay.java
@@ -0,0 +1,74 @@
+package model;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class ForecastDay {
+
+    //date is the key in Forecast
+    private float maxTempC;
+    private float maxTempF;
+    private float minTempC;
+    private float minTempF;
+    private float avgTempC;
+    private float avgTempF;
+    private float maxWindMph;
+    private float maxWindKph;
+    private float totalPrecipMm;
+    private float totalPrecipIn;
+    private float avgVisKm;
+    private float avgVisMiles;
+    private float avgHumidity;
+    private boolean dailyWillItRain;
+    private int dailyChanceOfRain; //percentage
+    private boolean dailyWillItSnow;
+    private int dailyChanceOfSnow; //percentage
+    private String condition;
+    private String iconUrl;
+    private float uv;
+
+    private HashMap<String, ForecastHour> hourlyWeather;
+
+    //astro
+    private String sunrise;
+    private String sunset;
+    private String moonPhase;
+
+    public ForecastDay(JSONObject jsonObject) {
+        maxTempC = jsonObject.getFloat("maxtemp_c");
+        maxTempF = jsonObject.getFloat("maxtemp_f");
+        minTempC = jsonObject.getFloat("mintemp_c");
+        minTempF = jsonObject.getFloat("mintemp_f");
+        avgTempC = jsonObject.getFloat("avgtemp_c");
+        avgTempF = jsonObject.getFloat("avgtemp_f");
+        maxWindKph = jsonObject.getFloat("maxwind_kph");
+        maxWindMph = jsonObject.getFloat("maxwind_mph");
+        totalPrecipMm = jsonObject.getFloat("totalprecip_mm");
+        totalPrecipIn = jsonObject.getFloat("totalprecip_in");
+        avgVisKm = jsonObject.getFloat("avgvis_km");
+        avgVisMiles = jsonObject.getFloat("avgvis_miles");
+        avgHumidity = jsonObject.getFloat("avghumidity");
+        dailyWillItRain =  jsonObject.getInt("daily_will_it_rain")==1;
+        dailyChanceOfRain = jsonObject.getInt("daily_chance_of_rain");
+        dailyWillItSnow =  jsonObject.getInt("daily_will_it_snow")==1;
+        dailyChanceOfSnow = jsonObject.getInt("daily_chance_of_snow");
+        condition = jsonObject.getJSONObject("condition").getString("text");
+        iconUrl = jsonObject.getJSONObject("condition").getString("icon");
+        uv = jsonObject.getFloat("uv");
+
+
+        JSONArray jsonArray = jsonObject.getJSONArray("hour");
+
+        for (Object o : jsonArray) {
+            ForecastHour fch = new ForecastHour((JSONObject) o);
+            this.hourlyWeather.put(((JSONObject) o).getString("time"), fch);
+        }
+
+        JSONObject astroObject = jsonObject.getJSONObject("astro");
+        this.sunrise = astroObject.getString("sunrise");
+        this.sunset = astroObject.getString("sunset");
+        this.moonPhase = astroObject.getString("moonPhase");
+    }
+}
